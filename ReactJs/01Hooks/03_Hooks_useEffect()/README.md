d352f1e5866361954b3997f68ad9502030f6485a
ayushb5 <179284688+ayushb5@users.noreply.github.com>
2025-12-10 19:03:41 +0530

    updated useEffect() with variable

--- ReactJs/01Hooks/03_Hooks_useEffect()/README.md
+++ ReactJs/01Hooks/03_Hooks_useEffect()/README.md
@@ -5,10 +5,20 @@ Output
 eg-
 
 useEffect(()=>{
-    document.title=`${count}`
+document.title=`${count}`
 })
 
 ![alt text](src/assets/output1.png)
 
 Output after click (increment counter)
 ![alt text](src/assets/output.png)
+
+Output for useEffect with Variable
+
+i.e
+
+useEffect(() => {
+document.title = `${otherCount}`
+}, [otherCount])
+
+![alt text](src/assets/varoutput.png)
--- ReactJs/01Hooks/03_Hooks_useEffect()/src/App.jsx
+++ ReactJs/01Hooks/03_Hooks_useEffect()/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [otherCount, setOtherCount] = useState(5)
 
   const increment = () => {
     setCount(count + 1)
@@ -12,15 +13,39 @@ function App() {
     setCount(count - 1)
   }
 
+  // variation 1- useEffect() without dependencies
+  // useEffect(() => {
+  //   document.title = `${count}`
+  // })
+
+  //output- It updates title because it runs on every render
+
+  //variation 2- useEffect() with emtpy array
+
+  // useEffect(() => {
+  //   document.title = `${count}`;
+  // }, [])
+
+  //output- It will not update title because it run only once (when component loads)
+
+  //variation 3- useEffect() with variable
+
   useEffect(() => {
-    document.title = `${count}`
-  })
+    document.title = `${otherCount}`
+  }, [otherCount])
+
+  //output- this will update only other count i.e multiple of 5 in title but not update title with {count}
+
 
   return (
     <div>
       <h1>{count} times clicked</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+
+      <br />
+      <h1>Other Counter: {otherCount}</h1>
+      <button onClick={() => { setOtherCount(otherCount + 5) }}>Increment by 5</button>
     </div>
   )
 }
